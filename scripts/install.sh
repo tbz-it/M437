fff6cd6e1c0d9f1412e77896c3402942a824e697
Thomas Kälin <kaelin@sweb.ch>
2021-03-01 13:33:17 +0100

    Secrets zuerst hinzufügen (apply)

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,5 +5,6 @@
 # TODO Datenbank auf $HOME/data/mysql = Rack Server ablegen
 
 export KUBECONFIG=$HOME/.kube/config
+kubectl apply -f ~/M437/secrets.yaml
 kubectl apply -f ~/M437/mysql.yaml
 kubectl apply -f ~/M437/osticket.yaml
