1d2b65308f50618a50c13034fda3a4cbf3512681
Armin Doerzbach <ado@doerzbach.com>
2021-09-10 23:06:14 +0200

    mysql.yaml: labels of mysql need to be different of labels of osticket, as the load balancer selects pods by labels -> sometimes msyql pod is selected as webserver which is not possible... osticket.yaml: remove clientIP stickiness again, as this was not the issue for the connection errors on webserver osticket. see above. install.sh: remove netfilter as it is not needed.

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,16 +6,9 @@
 # Create local directory in VM which contains the Mysql DB
 sudo mkdir /mysqldata
 sudo chown 999:999 /mysqldata
-sudo cp $(dirname $0)/netfilter /usr/local/bin
-sudo cp $(dirname $0)/netfilter.service /etc/systemd/system
-sudo chmod 755 /usr/local/bin/netfilter
-sudo systemctl daemon-reload
-sudo systemctl enable netfilter
 
 export KUBECONFIG=$HOME/.kube/config
 kubectl apply -f ~/M437/secrets.yaml
 kubectl apply -f ~/M437/mysql.yaml
 kubectl apply -f ~/M437/osticket.yaml
 
-sleep 60
-sudo reboot
